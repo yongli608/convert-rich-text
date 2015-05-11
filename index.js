6a8edf85622a4c7108fe21f3ca0d5f35645b04d1
Blake Thomson <thomsbg@gmail.com>
2015-05-11 10:41:46 -0400

    Refactor, kill trailing empty line hack, stop using forEach()

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Doc = require('./lib/doc');
 module.exports = function(delta, formats, options) {
   var doc = new Doc(formats, options);
   for (var i = 0; i < delta.ops.length; i++) {
-    doc.write(delta.ops[i]);
+    doc.writeOp(delta.ops[i]);
   }
   return doc.getHTML();
 };
