b9df66bf6b9bca243e7054c4593fdda6d5dba766
Blake Thomson <thomsbg@gmail.com>
2018-02-09 11:38:17 -0800

    Update for latest jsdom

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var jsdom = require('jsdom').jsdom;
+var jsdom = require('jsdom').JSDOM;
 var convert = require('./lib/convert');
 
 module.exports = function(delta, formats, options) {
-  options.document = jsdom();
+  options.document = new jsdom().window.document;
   return convert(delta, formats, options);
 };
 
